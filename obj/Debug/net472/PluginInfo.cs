3e3217ff9ef7b9362146b07148298a1d515c6eab
Royalty <royaltybusiness1999@gmail.com>
2025-06-07 20:28:42 +0200

    Delete obj directory

--- obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RoyalCommonalities
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.royalty.subnautica.RoyalCommonalities.mod";
-        public const string PLUGIN_NAME = "Roylaty's Commonalities";
-        public const string PLUGIN_VERSION = "0.1.8";
-    }
-}
